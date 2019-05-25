cc8563a21ca5697d1e16ffffbdc9d61d0a8b73eb
unknown <devlead@dynamisdevelopment.com>
2019-05-25 11:32:26 -0400

    Fixed Services Section

--- src/components/specializations/specializations.js
+++ src/components/specializations/specializations.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { graphql, useStaticQuery } from "gatsby"
 import Slider from "react-slick"
 import SectionTitle from "../sectionTitle/sectionTitle"
 
 const Specializations = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        allContentfulServices {
+            edges {
+                node {
+                    title
+                    thumbnail {
+                        file {
+                            url
+                        }
+                    }
+                }
+            }
+        }
+    }
+    `)
     var settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
@@ -13,71 +30,16 @@ const Specializations = () => {
     return (
         <div className="specializations-section">
             <SectionTitle section={"Services"} title={"Our Professional"} subtitle={"Specializations"} />
-            <Slider {...settings} slidesPerRow={4} className="standard-slider">
-                <a href="#" className="section one">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section two">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
-                <a href="#" className="section">
-                    <img src="./Assets/images/header-background.png" alt="" />
-                    <div className="text-wrapper">
-                        <h2>Architecture</h2>
-                        <h2>Design</h2>
-                        <p>Read more</p>
-                    </div>
-                </a>
+            <Slider {...settings} slidesPerRow={4} className="services-slider">
+                {data.allContentfulServices.edges.map((edge, index) => {
+                    return <a href="#" key={index} className="service-column">
+                        <img src={edge.node.thumbnail.file.url} />
+                        <div className="text-wrapper">
+                            <h2>{edge.node.title}</h2>
+                            <p>Read more <img src="./Assets/images/arrow.svg" /></p>
+                        </div>
+                    </a>
+                })}
             </Slider>
         </div>
     )
